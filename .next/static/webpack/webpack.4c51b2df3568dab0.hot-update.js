bc8631ca8cbdf9f618f9de3fecf7e923ddc0f504
Carlos Alberto <carampi@gmail.com>
2022-11-17 18:36:30 +0100

    next react y dependencias principales del front

---
+++ .next/static/webpack/webpack.4c51b2df3568dab0.hot-update.js
@@ -0,0 +1,29 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("webpack",{},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ !function() {
+/******/ 	__webpack_require__.h = function() { return "3df4e48fa44a759e"; }
+/******/ }();
+/******/ 
+/******/ /* webpack/runtime/make namespace object */
+/******/ !function() {
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/ }();
+/******/ 
+/******/ }
+);
---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const HomePage = () => {
+  return ( 
+    <div>
+        hola
+      {/* <Header />
+      <Widget />
+      <PrimerBloque />
+      <SegundoBloque />
+      <TercerBloque />
+      <CuartoBloque />
+      <QuintoBloque />
+      <SextoBloque />
+      <SeptimoBloque />
+      <Footer /> */}
+    </div>
+  );
+};
+
+export default HomePage;
