8b3ee6db8b92011f3eebcbd50569df3250ffb7ee
lTenryuu <cuau_daali@hotmail.com>
2022-11-24 07:52:29 -0600

    cambios a la vista de nota individual

---
+++ components/SeguirLeyendo/list.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Grid, GridItem, Box, Flex, Image, Text } from "@chakra-ui/react";
+import CardItem from "../../commons/SeguirLeyendo/cardItem";
+
+const List = () => {
+  const note = ["1", "2", "3", "4"];
+  return (
+    <Box boxSize={100}>
+      <CardItem />
+    </Box>
+  );
+};
+
+export default List;
--- pages/Subcategories/[url].jsx
+++ pages/Subcategories/[url].jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useRouter } from "next/router";
 import React, { useState, useEffect } from "react";
 import { Box, Text, Image, Divider, Heading } from "@chakra-ui/react";
+import List from "../../components/SeguirLeyendo/list";
 
 const Post = () => {
   const router = useRouter();
@@ -16,38 +17,48 @@ const Post = () => {
   return (
     <>
       {note ? (
-        <Box mr={300} ml={300}>
-          <Heading as="h1" fontSize={48} fontWeight="bold" textAlign="center">
-            {note.field_title_pre + note.field_title}
-          </Heading>
-          <Heading as="h2" fontSize={21} textAlign="center" fontWeight="normal">
-            {note.field_description}
-          </Heading>
-          <Divider
-            borderColor="#E32B6C"
-            borderStyle="solid"
-            borderWidth={2}
-            width={40}
-          />
-          <Text fontSize={16} textAlign="center">
-            por <b>{note.author}</b>.
-          </Text>
-          <Text fontSize={15} textAlign="center" color="#969696">
-            {note.createdAt}
-          </Text>
-          <Image
-            src={note.field_img_primary}
-            alt="Incendios forestales en el Río Paraná"
-            boxSize="900px"
-          />
-          {note.contents.map((itemContent, i) => (
-            <div
-              key={i}
-              className="text-container"
-              dangerouslySetInnerHTML={{ __html: itemContent.field_content }}
+        <>
+          <Box mr={300} ml={300}>
+            <Heading as="h1" fontSize={48} fontWeight="bold" textAlign="center">
+              {note.field_title_pre + note.field_title}
+            </Heading>
+            <Heading
+              as="h2"
+              fontSize={21}
+              textAlign="center"
+              fontWeight="normal"
+            >
+              {note.field_description}
+            </Heading>
+            <Divider
+              borderColor="#E32B6C"
+              borderStyle="solid"
+              borderWidth={2}
+              width={40}
             />
-          ))}
-        </Box>
+            <Text fontSize={16} textAlign="center">
+              por <b>{note.author}</b>.
+            </Text>
+            <Text fontSize={15} textAlign="center" color="#969696">
+              {note.createdAt}
+            </Text>
+            <Image
+              src={note.field_img_primary}
+              alt="Incendios forestales en el Río Paraná"
+              boxSize="900px"
+            />
+            {note.contents.map((itemContent, i) => (
+              <div
+                key={i}
+                className="text-container"
+                dangerouslySetInnerHTML={{ __html: itemContent.field_content }}
+              />
+            ))}
+          </Box>
+          <Box>
+            <List />
+          </Box>
+        </>
       ) : (
         <p>loading</p>
       )}
