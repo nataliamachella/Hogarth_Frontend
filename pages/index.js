563b1d45fc9c155fb840ba9ff63a943f45a1c1a5
francolivelli <franco.livelli@gmail.com>
2022-11-28 09:54:52 -0300

    maquetado inicial widget

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import BloqueA from "../components/BloqueA/BloqueA";
 import BloquePrincipal from "../components/BloquePrincipal/BloquePrincipal";
 import { ChakraProvider, Container, Spacer } from "@chakra-ui/react";
 import BloqueB from "../components/BloqueB/BloqueB";
+import Widget from "../components/Widget/Widget"
 
 const HomePage = () => {
   return (
@@ -13,8 +14,8 @@ const HomePage = () => {
         <BloquePrincipal />
         <BloqueA />
         <BloqueB />
+        <Widget />
         {/*
-      <Widget />
       <TercerBloque />
       <CuartoBloque />
       <QuintoBloque />
