8f141b60e077b77ff6cac02b1f11e0620fd460eb
nataliamachella <nataliamachella@gmail.com>
2022-11-29 13:54:23 -0300

    restaurando cambios

--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,17 @@ import BloquePrincipal from "../components/BloquePrincipal/BloquePrincipal";
 
 import BloqueD from "../components/BloqueD/BloqueD";
 import Navbar from "../components/Navbar/Navbar";
+import { Container } from "@chakra-ui/react";
 
 const HomePage = () => {
   return (
     <div>
       <Navbar />
-      <BloquePrincipal />
-      <BloqueA />
-      <BloqueD />
+      <Container display="flex" flexDirection="column">
+        <BloquePrincipal />
+        <BloqueA />
+        {/*  <BloqueD /> */}
+      </Container>
     </div>
   );
 };
