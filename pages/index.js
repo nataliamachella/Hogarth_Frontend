3f3ee70883f6af490599de9886b246105b56f8fe
nataliamachella <nataliamachella@gmail.com>
2022-11-29 13:42:58 -0300

    fonts added

--- pages/index.js
+++ pages/index.js
@@ -2,37 +2,17 @@ import React from "react";
 import BloqueA from "../components/BloqueA/BloqueA";
 import BloquePrincipal from "../components/BloquePrincipal/BloquePrincipal";
 
-import { ChakraProvider, Container, Spacer } from "@chakra-ui/react";
 import BloqueD from "../components/BloqueD/BloqueD";
+import Navbar from "../components/Navbar/Navbar";
 
 const HomePage = () => {
   return (
-    <ChakraProvider>
-      <div>
-        <Navbar />
-        <BloquePrincipal />
-        <BloqueA />
-        <BloqueD />
-        {/*
-import BloqueB from "../components/BloqueB/BloqueB";
-import Widget from "../components/Widget/Widget";
-import { Box } from "@chakra-ui/react";
-
-const HomePage = () => {
-  return (
-    <Box>
+    <div>
+      <Navbar />
       <BloquePrincipal />
       <BloqueA />
-      <BloqueB />
-      <Widget />
-      {/*
-      <TercerBloque />
-      <CuartoBloque />
-      <QuintoBloque />
-      <SextoBloque />
-      <SeptimoBloque />
-      <Footer /> */}
-    </Box>
+      <BloqueD />
+    </div>
   );
 };
 
