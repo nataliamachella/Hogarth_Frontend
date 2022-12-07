7f76df74c921dda2975d475c7de6acbf38530060
francolivelli <franco.livelli@gmail.com>
2022-12-07 10:01:25 -0300

    commit final

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,10 @@
 import Navbar from "../components/Navbar/Navbar";
 import { ChakraProvider } from "@chakra-ui/react";
-//import theme from "../utils/theme"
+import theme from "../utils/theme"
 import { useRouter } from "next/router";
 import "@fontsource/raleway/400.css";
 import "@fontsource/inter/700.css";
-import theme from "../public/fonts/theme";
+// import theme2 from "../public/fonts/theme";
 import SidebarAdmin from "../commons/Admin/SidebarAdmin";
 
 const MyApp = ({ Component, pageProps }) => {
